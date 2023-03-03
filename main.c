7b66b091f4d153f76a8ca7cad9d6d0a21d490e6e
BENOITON F <benoitonflorian+git@gmail.com>
2023-03-03 12:24:30 +0100

    Ajout déplacement paddle, collision entre la balle et le paddle et le mur

--- main.c
+++ main.c
@@ -8,7 +8,9 @@
 #define WINDOW_HEIGHT 320
 #define FPS 60
 
+#define PADDLE_HEIGHT 20
 #define PADDLE_WIDTH WINDOW_WIDTH/3
+#define PADDLE_SPEED 3
 #define BALL_RADIUS 9
 #define NB_LIGNE 4
 #define NB_BRIQUE 10
@@ -21,7 +23,7 @@ float paddle_x;
 float paddle_y;
 float ball_x;
 float ball_y;
-float ball_speed_x = 0;
+float ball_speed_x = -2;
 float ball_speed_y = -1;
 
 int initBricks();
@@ -64,7 +66,7 @@ void drawBall() {
 
 void drawPaddle() {
     changeColor(0,0,255);
-    drawRect(paddle_x, paddle_y, PADDLE_WIDTH, 20);
+    drawRect(paddle_x, paddle_y, PADDLE_WIDTH, PADDLE_HEIGHT);
 }
 
 void drawBricks() {
@@ -94,14 +96,20 @@ void drawGame() {
 
 /**
  * Moves the paddle along the x-axis at the given speed
- * @param speed (-1 : to the left | 1 : to the right)
+ * @param direction (-1 : to the left | 1 : to the right)
 */
-void MovePaddle(int speed) {
-    paddle_x += speed;
-    if (paddle_x < 0)
+void MovePaddle(int direction) {
+    if (direction == -1) {
+        paddle_x -= PADDLE_SPEED;
+    } else if (direction == 1) {    
+        paddle_x += PADDLE_SPEED;
+    }
+    if (paddle_x < 0) {
         paddle_x = 0;
-    else if (paddle_x + PADDLE_WIDTH > WINDOW_WIDTH)
+    }
+    else if (paddle_x + PADDLE_WIDTH > WINDOW_WIDTH) {
         paddle_x = WINDOW_WIDTH - PADDLE_WIDTH;
+    }
 }
 
 void KeyPressed(SDL_Keycode touche) {
@@ -126,6 +134,110 @@ void joyButtonPressed() {
 
 }
 
+/**
+ * Collision between ball and wall
+*/
+void WallCollision() {
+    //Top wall
+    if (ball_y - BALL_RADIUS <= 0) {
+        ball_speed_y = -ball_speed_y;
+        ball_y = BALL_RADIUS;
+    }
+    //Bot wall
+    if (ball_y + BALL_RADIUS >= WINDOW_HEIGHT) {
+        //LOSE
+    }
+    
+    //Left wall 
+    if (ball_x - BALL_RADIUS <= 0) {
+        ball_speed_x = -ball_speed_x;
+        ball_x = BALL_RADIUS;
+    }
+    //Right wall
+    if (ball_x + BALL_RADIUS >= WINDOW_WIDTH) {
+        ball_speed_x = -ball_speed_x;
+        ball_x = WINDOW_WIDTH - BALL_RADIUS;
+    }
+}
+
+void PaddleCollision() {
+    //Position relative of the ball
+    int top_ball = ball_y - BALL_RADIUS;
+    int bot_ball = ball_y + BALL_RADIUS;
+    int left_ball = ball_x - BALL_RADIUS;
+    int right_ball = ball_x + BALL_RADIUS;
+
+    //Position relative of the paddle
+    int top_paddle = paddle_y;
+    int bot_paddle = paddle_y + PADDLE_HEIGHT;
+    int left_paddle = paddle_x;
+    int right_paddle = paddle_x + PADDLE_WIDTH;
+    int third_paddle_width = PADDLE_WIDTH / 3;
+
+    //
+    int change_speed_x = 0, change_speed_y = 0;
+
+    if (bot_ball - top_paddle >= 0 && bot_ball - top_paddle <= ball_speed_y) {
+        // ball hit top left side
+        if (ball_x >= left_paddle && ball_x <= left_paddle + third_paddle_width) { 
+            if (ball_speed_x != 0)
+                ball_speed_x = -abs(ball_speed_x);
+            else
+                ball_speed_x = -1;
+        }
+        // ball hit top middle side
+        else if (ball_x >= left_paddle + third_paddle_width && ball_x <= left_paddle + third_paddle_width * 2) {
+            //printf("Deuxième tier\n");
+        }
+        // ball hit top right side
+        else if (ball_x >= left_paddle + third_paddle_width * 2 && ball_x <= right_paddle) {
+            if (ball_speed_x != 0)
+                ball_speed_x = abs(ball_speed_x);
+            else
+                ball_speed_x = 1;
+        }
+    }
+    
+    //Axe Vertical
+    if (right_ball >= left_paddle && left_ball <= right_paddle) {
+        //Top collision
+        if (bot_ball - top_paddle >= 0 && bot_ball - top_paddle <= ball_speed_y) {
+            ball_speed_y = -ball_speed_y;
+        }
+        // Bottom collision
+        else if (top_ball - bot_paddle <= 0 && top_ball - bot_paddle >= ball_speed_y) {
+            ball_speed_y = -ball_speed_y;
+        }
+    }
+
+    //Axe Horizontal
+    if (bot_ball >= top_paddle && top_ball <= bot_paddle) {
+        //Left collision
+        if (right_ball - left_paddle >= 0 && right_ball - left_paddle <= abs(ball_speed_x) + PADDLE_SPEED) {
+            ball_x = left_paddle - BALL_RADIUS;
+            change_speed_x = 1;
+        } 
+        //Right collision
+        else if (left_ball - right_paddle <= 0 && left_ball - right_paddle >= abs(ball_speed_x) - PADDLE_SPEED) {
+            ball_x = right_paddle + BALL_RADIUS;
+            change_speed_x = 1;
+        }
+    }
+
+    if (change_speed_y)
+        ball_speed_y = -ball_speed_y;
+    if (change_speed_x)
+        ball_speed_x = -ball_speed_x;
+}
+
+void moveBall() {
+    ball_x += ball_speed_x;
+    ball_y += ball_speed_y;
+
+    WallCollision();
+    PaddleCollision();
+}
+
 void gameLoop() {
     int programLaunched = 1;
     while (programLaunched == 1) {
@@ -148,7 +260,7 @@ void gameLoop() {
                     break;
             }
         }
-        
+        moveBall();
         drawGame();
     }
 }
